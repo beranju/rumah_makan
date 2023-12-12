8fdbea0475f34564df872d25f29bd1ba9ddc89fa
beranju <beranjusihombing1@gmail.com>
2023-12-12 13:27:22 +0700

    create splash page

---
+++ lib/theme/assets_manager.dart
@@ -0,0 +1,6 @@
+class Images{
+  static const String path = 'assets/images';
+
+  static const String appLogo = "$path/app_logo.png";
+  static const String splashLogo = "$path/splash_logo.png";
+}
--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -1,11 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
-  static const String routeName ='/splash_page';
+import 'package:flutter/material.dart';
+import 'package:rumah_makan/theme/assets_manager.dart';
+import 'package:rumah_makan/ui/home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  static const String routeName = '/splash_page';
+
   const SplashPage({super.key});
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<Timer> _navigateToHome() async {
+    const second = 3;
+    const duration = Duration(seconds: second);
+    return Timer(duration, () {
+      Navigator.pushReplacementNamed(context, HomePage.routeName);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: Center(
+        child: Container(
+          width: 200.0,
+          height: 200.0,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Images.splashLogo),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
