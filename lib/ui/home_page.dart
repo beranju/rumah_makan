fd5b19db6d53a837f0879d7274a94607ff1196ec
beranju <beranjusihombing1@gmail.com>
2023-12-16 13:16:08 +0700

    add shimmer when load data

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rumah_makan/common/result_state.dart';
@@ -5,7 +6,10 @@ import 'package:rumah_makan/data/api/api_service.dart';
 import 'package:rumah_makan/data/model/restaurant.dart';
 import 'package:rumah_makan/provider/restaurant_provider.dart';
 import 'package:rumah_makan/ui/detail_page.dart';
+import 'package:rumah_makan/ui/widget/platform_widget.dart';
 import 'package:rumah_makan/ui/widget/restaurant_item.dart';
+import 'package:rumah_makan/ui/widget/shimmer_list.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/';
@@ -17,22 +21,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Restaurant> listRestaurant = [];
   final TextEditingController _controller = TextEditingController();
   final FocusNode _focusNode = FocusNode();
-  String? error;
-  bool dataNotFound = false;
 
   @override
   void initState() {
-    _loadData();
     super.initState();
-  }
-
-  @override
-  void didUpdateWidget(covariant HomePage oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _loadData();
   }
 
   @override
@@ -41,155 +35,125 @@ class _HomePageState extends State<HomePage> {
     _focusNode.unfocus();
     super.dispose();
   }
-
-  Future<void> _loadData() async {
-    // try {
-    //   String json = await rootBundle.loadString(DataSource.dataSource);
-    //   List<Restaurant> restaurants = restaurantFromJson(json);
-    //   setState(
-    //     () {
-    //       dataNotFound = (restaurants.isEmpty) ? true : false;
-    //       listRestaurant = restaurants;
-    //     },
-    //   );
-    // } catch (e) {
-    //   setState(() {
-    //     error = 'Failed to load the data, try again';
-    //   });
-    // }
-  }
-
-  void _findByName(String name) {
-    List<Restaurant> filteredRestaurant = listRestaurant
-        .where((restaurant) => (restaurant.name != null)
-            ? restaurant.name!.toLowerCase().contains(name)
-            : false)
-        .toList();
-    setState(
-      () {
-        dataNotFound = (filteredRestaurant.isEmpty) ? true : false;
-        listRestaurant = filteredRestaurant;
+  
+  Widget _listRestaurant(BuildContext context, List<Restaurant> restaurant){
+    return ListView.builder(
+      itemCount: restaurant.length,
+      itemBuilder: (context, index) {
+        final rest = restaurant[index];
+        return RestaurantItem(
+          restaurant: rest,
+          onTap: () {
+            Navigator.pushNamed(context, DetailPage.routeName,
+                arguments: rest);
+          },
+        );
       },
     );
+  }
+
+  Widget _buildList() {
+    return Consumer<RestaurantProvider>(
+      builder: (context, state, v) {
+        if (state.state == ResultState.loading) {
+          return const ShimmerList();
+        } else if (state.state == ResultState.hasData) {
+          final rest = state.restaurant;
+          return _listRestaurant(context, state.restaurant);
+        } else if (state.state == ResultState.noData) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else if (state.state == ResultState.error) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text(''),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: false, title: const Text("Rumah Makan")),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 12.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              width: double.infinity,
+              height: 75.0,
+              child: Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      controller: _controller,
+                      textInputAction: TextInputAction.search,
+                      onTapOutside: (event) {
+                        _focusNode.unfocus();
+                      },
+                      onSubmitted: (value) {
+                      },
+                      decoration: InputDecoration(
+                        fillColor: Theme.of(context).colorScheme.surface,
+                        suffixIcon: _controller.text.isNotEmpty
+                            ? GestureDetector(
+                                child: const Icon(
+                                  Icons.close,
+                                ),
+                              )
+                            : null,
+                        hintText: 'Find restaurant',
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (_controller.text.isNotEmpty) {
+                        // _findByName(_controller.text);
+                        _focusNode.unfocus();
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.search,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: _buildList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(child: _buildList());
   }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => RestaurantProvider(apiService: ApiService()),
-      child: Scaffold(
-        appBar: AppBar(
-            automaticallyImplyLeading: false, title: const Text("Rumah Makan")),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 12.0),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                width: double.infinity,
-                height: 75.0,
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: TextField(
-                        controller: _controller,
-                        textInputAction: TextInputAction.search,
-                        onTapOutside: (event) {
-                          _focusNode.unfocus();
-                        },
-                        onSubmitted: (value) {
-                          if (value.isNotEmpty) {
-                            _findByName(value);
-                            _focusNode.unfocus();
-                          } else {
-                            _loadData();
-                          }
-                        },
-                        decoration: InputDecoration(
-                          fillColor: Theme.of(context).colorScheme.surface,
-                          suffixIcon: _controller.text.isNotEmpty
-                              ? GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      dataNotFound = false;
-                                    });
-                                    _controller.clear();
-                                    _loadData();
-                                  },
-                                  child: const Icon(Icons.close))
-                              : null,
-                          hintText: 'Find restaurant',
-                          filled: true,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          if (_controller.text.isNotEmpty) {
-                            _findByName(_controller.text);
-                            _focusNode.unfocus();
-                          }
-                        },
-                        icon: const Icon(Icons.search))
-                  ],
-                ),
-              ),
-              Expanded(
-                child: (error == null)
-                    ? (dataNotFound)
-                        ? const Center(child: Text('Restaurant not found :)'))
-                        : Consumer<RestaurantProvider>(
-                            builder: (context, state, v) {
-                              if (state.state == ResultState.loading) {
-                                return const Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              } else if (state.state == ResultState.hasData) {
-                                final rest = state.restaurant;
-                                return ListView.builder(
-                                  itemCount: state.restaurant.length,
-                                  itemBuilder: (context, index) {
-                                    final restaurant = rest[index];
-                                    return RestaurantItem(
-                                      restaurant: restaurant,
-                                      onTap: () {
-                                        Navigator.pushNamed(
-                                            context, DetailPage.routeName,
-                                            arguments: restaurant);
-                                      },
-                                    );
-                                  },
-                                );
-                              } else if (state.state == ResultState.noData) {
-                                return Center(
-                                  child: Text(state.message),
-                                );
-                              } else if (state.state == ResultState.error) {
-                                return Center(
-                                  child: Material(
-                                    child: Text(state.message),
-                                  ),
-                                );
-                              } else {
-                                return const Center(
-                                  child: Text(''),
-                                );
-                              }
-                            },
-                          )
-                    : Center(
-                        child: Text(
-                          error!,
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                      ),
-              ),
-            ],
-          ),
-        ),
+      child: PlatformWidget(
+        androidBuilder: _buildAndroid,
+        iosBuilder: _buildIos,
       ),
     );
   }
